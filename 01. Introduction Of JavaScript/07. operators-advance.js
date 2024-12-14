17abbc8d867fa233f8350658fbc98340dc684ddf
Barkatzx <barkatullah.zx@gmail.com>
2024-12-14 12:32:24 +0600

    Operator Advance

---
+++ 01. Introduction Of JavaScript/07. operators-advance.js
@@ -0,0 +1,41 @@
+// JavaScript provides several shorthand notations to simplify mathematical operations and make code more concise and readable. These shorthand operators are often used to modify a variable with a mathematical operation, saving the need for verbose expressions.
+
+// 1. Addition Assignment (+=) : The addition assignment operator (+=) allows you to add a value to a variable and assign the result to the same variable.
+var score = 10;
+score += 5; // score = score + 5;
+console.log(score); // 15
+
+// 2. Subtraction Assignment (-=): The subtraction assignment operator (-=) subtracts a value from a variable and assigns the result to the same variable.
+var balance = 100;
+balance -= 30; // balance = balance - 30;
+console.log(balance); // 70
+
+// 3. Multiplication Assignment (*=) : The multiplication assignment operator (*=)multiplies a variable by a value and assigns the result to the same variable.
+var price = 20;
+price *= 5; //price = price * 2;
+console.log(price); // 100
+
+// 4. Division Assignment (/=) : The division assignment operator (/=) divides a variable by a value and assigns the result to the same variable.
+var total = 100;
+total /= 5; //total = total / 5;
+console.log(total); //20
+
+// 5. Modulus Assignment (%=): The modulus assignment operator (%=) calculates the remainder when a variable is divided by a value and assigns the result to the same variable.
+var number = 10;
+number %= 3; // number = number % 3;
+console.log(number); // 1
+
+// 6. Exponentiation Assignment (**=) : The exponentiation assignment operator (**=) raises a variable to the power of a value and assigns the result to the same variable.
+var base = 2;
+base **= 3; //base = base ** 3;
+console.log(base); // 8
+
+// 7. Increment (++) : The increment operator (++) increases a variable’s value by 1.
+var count = 0;
+count++; //count = count + 1;
+console.log(count);
+
+// 8. Decrement (--) : The decrement operator (--) decreases a variable’s value by 1.
+var count = 10;
+count--;
+console.log(count); // 9
