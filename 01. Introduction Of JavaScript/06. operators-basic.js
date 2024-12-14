a660c9931ef64d5336e032cd8f1b0e95035418fa
Barkatzx <barkatullah.zx@gmail.com>
2024-12-14 11:59:54 +0600

    Operator Basic

---
+++ 01. Introduction Of JavaScript/06. operators-basic.js
@@ -0,0 +1,34 @@
+// Arithmetic operators in JavaScript are used to perform mathematical operations on numbers. These operators allow you to perform basic calculations such as addition, subtraction, multiplication, division, and more.
+
+// 01. Addition (+) : The addition operator is used to add two operands.
+var sum = 5 + 5;
+console.log(sum); // 10
+
+var concatString = "Hello" + "World";
+console.log(concatString); // Hello World
+
+// 2. Subtraction (-) : The subtraction operator is used to subtract one operand from another.
+var number = 10 - 5;
+console.log(number); // 5
+
+// 3. Multiplication (*) : The multiplication operator is used to multiply two operands.
+var product = "6 * 4";
+console.log(product); // 24
+
+// 4. Division (/) : The division operator is used to divide one operand by another.
+var price = 10/2;
+console.log(price); //5
+
+// 5. Modulus (%) : The modulus operator returns the remainder of a division operation.
+var num = 10 % 3;
+console.log(num); // 1
+
+// Summary of Arithmetic Operators in JavaScript
+
+// +	Addition	5 + 3	8
+// -	Subtraction	10 - 5	5
+// *	Multiplication	6 * 4	24
+// /	Division	10 / 2	5
+// %	Modulus (remainder)	10 % 3	1
+// **	Exponentiation (power)	2 ** 3	8
+// - (Unary)	Unary negation (negates the value)	-5	-5
